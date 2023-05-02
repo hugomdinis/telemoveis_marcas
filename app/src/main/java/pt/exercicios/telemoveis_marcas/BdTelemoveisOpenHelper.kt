8fe8268ba02d305b77fc0da9ced2ce221efb32c1
hugomdinis <hugomdinis@hotmail.com>
2023-05-02 10:40:16 +0100

    Definir o nome da base de dados

--- app/src/main/java/pt/exercicios/telemoveis_marcas/BdTelemoveisOpenHelper.kt
+++ app/src/main/java/pt/exercicios/telemoveis_marcas/BdTelemoveisOpenHelper.kt
@@ -4,17 +4,18 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
+private const val NOME_BASE_DADOS = "Telemoveis:db"
+private const val VERSÃO_BASE_DADOS = 1
+
+
 class BdTelemoveisOpenHelper(
     context: Context?,
     name: String?,
-    factory: SQLiteDatabase.CursorFactory?,
     version: Int
-) : SQLiteOpenHelper(context, name, factory, version) {
+) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSÃO_BASE_DADOS) {
     override fun onCreate(p0: SQLiteDatabase?) {
         TODO("Not yet implemented")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
-    }
 }
