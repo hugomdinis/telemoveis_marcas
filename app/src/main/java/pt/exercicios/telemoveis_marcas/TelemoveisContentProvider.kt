f53eee5f594637aa813504e7245354fdd61f0075
hugomdinis <hugomdinis@hotmail.com>
2023-05-23 09:51:25 +0100

    Implementado o metodo query no content provider

--- app/src/main/java/pt/exercicios/telemoveis_marcas/TelemoveisContentProvider.kt
+++ app/src/main/java/pt/exercicios/telemoveis_marcas/TelemoveisContentProvider.kt
@@ -5,23 +5,47 @@ import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import android.provider.BaseColumns
 
-class TelemoveisContentProvider : ContentProvider(){
+class TelemoveisContentProvider : ContentProvider() {
 
-    private var bdOpenHelper : BdTelemoveisOpenHelper? = null
+    private var bdOpenHelper: BdTelemoveisOpenHelper? = null
     override fun onCreate(): Boolean {
         bdOpenHelper = BdTelemoveisOpenHelper(context)
         return true
     }
 
     override fun query(
-        p0: Uri,
-        p1: Array<out String>?,
-        p2: String?,
-        p3: Array<out String>?,
-        p4: String?
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
     ): Cursor? {
-        TODO("Not yet implemented")
+        val bd = bdOpenHelper!!.readableDatabase
+
+        val endereco = UriMatcher().match(uri)
+        val tabela = when (endereco) {
+            URI_MARCA, URI_MARCA_ID -> TabelaMarca(bd)
+            URI_TELEMOVEIS, URI_TELEMOVEL_ID -> TabelaTelemoveis(bd)
+            else -> null
+        }
+
+        val id = uri.lastPathSegment
+
+        val selecao = when (endereco) {
+            URI_MARCA_ID, URI_TELEMOVEL_ID -> Pair("${BaseColumns._ID}=?",arrayOf(id))
+            else -> Pair(selection,selectionArgs)
+        }
+
+        return tabela?.consulta(
+                projection as Array<String>,
+                selecao,
+                selectionArgs as Array<String>?,
+                null,
+                null,
+                sortOrder)
+
     }
 
     override fun getType(p0: Uri): String? {
@@ -47,13 +71,21 @@ class TelemoveisContentProvider : ContentProvider(){
         const val  TELEMOVEIS = "Telemovel"
 
         private const val URI_MARCA = 100
+        private const val URI_MARCA_ID = 101
         private const val URI_TELEMOVEIS = 200
+        private const val URI_TELEMOVEL_ID = 201
+
 
         fun UriMatcher() = UriMatcher(UriMatcher.NO_MATCH).apply {
             addURI(AUTORIDADE, MARCA, URI_MARCA)
+            addURI(AUTORIDADE, "$MARCA/#", URI_MARCA_ID)
             addURI(AUTORIDADE, TELEMOVEIS, URI_TELEMOVEIS)
+            addURI(AUTORIDADE, "$TELEMOVEIS/#", URI_TELEMOVEL_ID)
+
 
         }
+
+        // pt.exercicios.telemoveis_marcas
     }
 
 }
