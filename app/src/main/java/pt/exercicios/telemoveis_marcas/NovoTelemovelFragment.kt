9e8dfa636d1f2d0f344ec90fafe13d2913c4c35b
hugomdinis <hugomdinis@hotmail.com>
2023-06-06 10:43:27 +0100

    Adicionar fragmento para adicionar um novo telemovel

---
+++ app/src/main/java/pt/exercicios/telemoveis_marcas/NovoTelemovelFragment.kt
@@ -0,0 +1,44 @@
+package pt.exercicios.telemoveis_marcas
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import pt.exercicios.telemoveis_marcas.databinding.FragmentNovoTelemovelBinding
+import pt.exercicios.telemoveis_marcas.databinding.FragmentSobreBinding
+
+
+class NovoTelemovelFragment : Fragment() {
+    private var _binding: FragmentNovoTelemovelBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentNovoTelemovelBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_main
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
