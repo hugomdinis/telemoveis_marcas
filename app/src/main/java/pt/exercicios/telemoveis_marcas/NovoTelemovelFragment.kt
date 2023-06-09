97f765094558fb1cbf029f99518c39663d1c4a52
hugomdinis <hugomdinis@hotmail.com>
2023-06-09 11:39:12 +0100

    Mostrar os dados das marcas no spinner (novo telemovel)

--- app/src/main/java/pt/exercicios/telemoveis_marcas/NovoTelemovelFragment.kt
+++ app/src/main/java/pt/exercicios/telemoveis_marcas/NovoTelemovelFragment.kt
@@ -1,18 +1,22 @@
 package pt.exercicios.telemoveis_marcas
 
 
+import android.database.Cursor
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.LoaderManager
+import android.support.v4.content.CursorLoader
+import android.support.v4.content.Loader
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.SimpleCursorAdapter
 import pt.exercicios.telemoveis_marcas.databinding.FragmentNovoTelemovelBinding
-import pt.exercicios.telemoveis_marcas.databinding.FragmentSobreBinding
 
+private const val ID_LOADER_MARCAS = 0
 
-class NovoTelemovelFragment : Fragment() {
+class NovoTelemovelFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     private var _binding: FragmentNovoTelemovelBinding? = null
 
     // This property is only valid between onCreateView and
@@ -32,6 +36,8 @@ class NovoTelemovelFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val loader = LoaderManager.getInstance(this)
+        loader.initLoader(ID_LOADER_MARCAS, null , this)
         val activity = activity as MainActivity
         activity.fragment = this
         activity.idMenuAtual = R.menu.menu_guardar_cancelar
@@ -62,6 +68,36 @@ class NovoTelemovelFragment : Fragment() {
 
     private fun guardar() {
         TODO("Not yet implemented")
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(
+            requireContext(),
+            TelemoveisContentProvider.ENDERECO_MARCA,
+            TabelaMarca.TODOS_OS_CAMPOS,
+            null, null,
+            TabelaMarca.CAMPO_NOME
+        )
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        binding.spinnerMarca.adapter = null
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if (data == null) {
+            binding.spinnerMarca.adapter = null
+            return
+        }
+
+        binding.spinnerMarca.adapter = SimpleCursorAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            data,
+            arrayOf(TabelaMarca.CAMPO_NOME),
+            intArrayOf(android.R.id.text1),
+            0
+        )
     }
 
 }
