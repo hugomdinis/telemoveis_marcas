999103e9f32908e12ae20f9b869c9ae4d1e58f90
hugomdinis <hugomdinis@hotmail.com>
2023-06-13 09:18:07 +0100

    Criação fragmento para eliminar Telemoveis

---
+++ app/src/main/java/pt/exercicios/telemoveis_marcas/EliminarTelemovelFragment.kt
@@ -0,0 +1,11 @@
+package pt.exercicios.telemoveis_marcas
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EliminarTelemovelFragment : Fragment() {
+
+}
