00e93a677f32deec1e211e00fcd0c6813a1f7105
hugomdinis <hugomdinis@hotmail.com>
2023-06-13 13:47:47 +0100

    Adicionada fragmento para eliminar telemoveis e correspondente menu

--- app/src/main/java/pt/exercicios/telemoveis_marcas/EliminarTelemovelFragment.kt
+++ app/src/main/java/pt/exercicios/telemoveis_marcas/EliminarTelemovelFragment.kt
@@ -3,9 +3,60 @@ package pt.exercicios.telemoveis_marcas
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import pt.exercicios.telemoveis_marcas.databinding.FragmentEliminarTelemovelBinding
 
 class EliminarTelemovelFragment : Fragment() {
+    private var _binding: FragmentEliminarTelemovelBinding? = null
 
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentEliminarTelemovelBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_eliminar
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_eliminar -> {
+                eliminar()
+                true
+            }
+            R.id.action_cancelar -> {
+                voltaListaLivros()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun voltaListaLivros() {
+        findNavController().navigate(R.id.action_eliminarTelemovelFragment_to_ListaDeTelemoveisFragment)
+    }
+
+    private fun eliminar() {
+    }
 }
