4f16977c5916e4c68e1700a6f604a6eb57e90966
hugomdinis <hugomdinis@hotmail.com>
2023-06-19 00:09:19 +0100

    Implementação do editar/guardar marca(com erros)

--- app/src/main/java/pt/exercicios/telemoveis_marcas/EditarMarcaFragment.kt
+++ app/src/main/java/pt/exercicios/telemoveis_marcas/EditarMarcaFragment.kt
@@ -1,5 +1,6 @@
 package pt.exercicios.telemoveis_marcas
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,6 +14,7 @@ import pt.exercicios.telemoveis_marcas.databinding.FragmentEditarMarcaBinding
 
 class EditarMarcaFragment : Fragment() {
 
+    private var marcas: Marca?= null
     private var _binding: FragmentEditarMarcaBinding? = null
 
     private val binding get () = _binding!!
@@ -31,6 +33,18 @@ class EditarMarcaFragment : Fragment() {
         val activity = activity as MainActivity
         activity.fragment = this
         activity.idMenuAtual = R.menu.menu_guardar_cancelar
+
+        val marca = EditarMarcaFragmentArgs.fromBundle(requireArguments()).marcas
+
+        if (marca != null){
+            activity.atualizaModelo(R.string.editar_marca)
+
+            binding.editTextNomeMarca.setText(marca.nome_marca)
+        }else{
+            activity.atualizaModelo(R.string.nova_marca_label)
+        }
+
+        this.marcas = marca
     }
 
     override fun onDestroyView() {
@@ -60,16 +74,47 @@ class EditarMarcaFragment : Fragment() {
             return
         }
 
-        val marca = Marca(nomeMarca)
+        if (marcas == null){
+            val marca = Marca(nomeMarca)
 
-        requireActivity().contentResolver.insert(TelemoveisContentProvider.ENDERECO_MARCA, marca.toContentValues())
+            insereMarca(marca)
+        }else{
+            val marca = marcas!!
+            marca.nome_marca = nomeMarca
 
-        if (id == null){
+            alteraMarca(marca)
+        }
+    }
+
+    private fun alteraMarca(marca: Marca) {
+        val enderecoMarca = Uri.withAppendedPath(TelemoveisContentProvider.ENDERECO_MARCA, marca.idMarca.toString())
+        val MarcasAlteradas = requireActivity().contentResolver.update(enderecoMarca, marca.toContentValues(), null, null)
+
+        if(MarcasAlteradas == 1){
+            Toast.makeText(requireContext(), getString(R.string.marca_alterada_com_sucesso), Toast.LENGTH_LONG).show()
+            voltarlistaMarcas()
+        }else{
             binding.editTextNomeMarca.error = getString(R.string.imposivel_guardar_marca)
+        }
+    }
+
+    private fun insereMarca(marca: Marca) {
+        val id = requireActivity().contentResolver.insert(
+            TelemoveisContentProvider.ENDERECO_MARCA,
+            marca.toContentValues()
+        )
+
+        if (id == null) {
+            binding.editTextNomeMarca.error =
+                getString(R.string.imposivel_guardar_marca)
             return
         }
 
-        Toast.makeText(requireContext(), getString(R.string.marca_guardada), Toast.LENGTH_LONG).show()
+        Toast.makeText(
+            requireContext(),
+            getString(R.string.marca_guardada),
+            Toast.LENGTH_LONG
+        ).show()
         voltarlistaMarcas()
     }
 
