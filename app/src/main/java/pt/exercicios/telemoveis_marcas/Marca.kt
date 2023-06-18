f656e57f9f433a264c96c14047f3d22c988613d6
hugomdinis <hugomdinis@hotmail.com>
2023-06-19 00:11:31 +0100

    Correção do erro do Serializable

--- app/src/main/java/pt/exercicios/telemoveis_marcas/Marca.kt
+++ app/src/main/java/pt/exercicios/telemoveis_marcas/Marca.kt
@@ -3,11 +3,12 @@ package pt.exercicios.telemoveis_marcas
 import android.content.ContentValues
 import android.database.Cursor
 import android.provider.BaseColumns
+import java.io.Serializable
 
 data class Marca(
     var nome_marca: String,
     var idMarca: Long = -1
-) {
+) : Serializable {
     fun toContentValues() : ContentValues {
         val valores = ContentValues()
 
